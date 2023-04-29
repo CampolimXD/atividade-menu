55c0aecab9c0debddf4663f658e6d31a249c6633
Ian Reis <68599564+IRe1s@users.noreply.github.com>
2023-04-29 16:37:11 -0300

    Adicionando Script

---
+++ script.js
@@ -0,0 +1,14 @@
+
+/**window.onscroll = function() {myFunction()};
+
+var nav = document.getElementById("navbar");
+var sticky = nav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+ **/
